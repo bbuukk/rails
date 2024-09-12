8f5f01d81c463d466e23d9e2bc9d2d6e7a43ab27
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-09-12 16:52:36 +0300

    feat(auth): let user sign in|up|out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "home", to: "pages#home", as: :home
+  get 'home', to: 'pages#home', as: :home
 
   resources :users
+  delete 'logout', to: 'users#log_out'
+
+  post 'login', to: 'users#log_in'
+
   resources :tasks
 
-  root to: "pages#home"
+  root to: 'users#new'
 end
