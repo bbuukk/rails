941daa2add4688dd9b0442ec0b7cc1a76542ddee
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-09-12 16:52:36 +0300

    feat(auth): let user sign in|up|out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,5 @@
 class PagesController < ApplicationController
   def home
     @user = User.find_by(id: session[:user_id]) if session[:user_id]
-
-    @time = Time.now
-
-    @files = Dir.entries('./')
   end
 end
