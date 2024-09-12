13ab1d2538c28c4dcf23ff965caca8976f791d53
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-09-12 14:37:35 +0300

    fix(UserSetting model): delete default values on init from model *chore(tmp and logs): delete reduntant files from git *feat(userSettings model): add defaults to model fields *fix(linting): correct linting error *fix(test): fix url_path in test *fix: corrected linting error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_11_132534) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_12_113831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,9 +25,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_11_132534) do
 
   create_table "user_settings", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "notification"
-    t.string "color"
-    t.string "theme"
+    t.string "notification", default: "email"
+    t.string "color", default: "blue"
+    t.string "theme", default: "light"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_settings_on_user_id"
