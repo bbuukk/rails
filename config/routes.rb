7478ad4f122969876c27b6be7b94a6020dbce231
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-09-13 13:47:45 +0300

    refactor(auth): use simple_form gem for sign in|up

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "home", to: "pages#home", as: :home
+  get 'home', to: 'pages#home', as: :home
 
-  resources :users, only: %i[new create destroy]
-  post "login", to: "users#log_in"
-  delete "logout", to: "users#log_out"
+  resources :users, only: %i[new create destroy] do
+    collection do
+      post 'log_in'
+      delete 'log_out'
+    end
+  end
 
   resources :tasks
 
-  root to: "users#new"
+  root to: 'users#new'
 end
