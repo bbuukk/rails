fff2380886964be7bb821bc7fea298c10bc0e2d8
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-09-13 18:09:43 +0300

    refactor(api)!: split sign_in and sign_up to seperate pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'home', to: 'pages#home', as: :home
+  resources :users, only: %i[new create destroy]
 
-  resources :users, only: %i[new create destroy] do
-    collection do
-      post 'log_in'
-      delete 'log_out'
-    end
-  end
+  get "/sign_in", to: "sessions#new_sign_in"
+  post "/sign_in", to: "sessions#create_sign_in"
+
+  delete "/sign_out", to: "sessions#sign_out"
+
+  get "/sign_up", to: "sessions#new_sign_up"
+  post "/sign_up", to: "sessions#create_sign_up"
 
   resources :tasks
 
-  root to: 'users#new'
+  root to: "pages#home"
 end
