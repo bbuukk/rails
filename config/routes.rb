5dd1531bec80fbd23df78982dbb119859b02971c
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-09-17 12:55:32 +0300

    fix: set old name for sign_up route for backwards compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create destroy]
 
-  get '/sign_in', to: 'sessions#new_sign_in'
-  post '/sign_in', to: 'sessions#create_sign_in'
+  get "/sign_in", to: "sessions#new_sign_in"
+  post "/sign_in", to: "sessions#create_sign_in"
 
-  delete '/sign_out', to: 'sessions#sign_out'
+  delete "/sign_out", to: "sessions#sign_out"
 
-  get '/users', to: 'sessions#new_sign_up'
-  post '/sign_up', to: 'sessions#create_sign_up'
+  get "/users", to: "sessions#new_sign_up", as: :sign_up
+  post "/sign_up", to: "sessions#create_sign_up"
 
   resources :tasks
 
-  root to: 'pages#home'
+  root to: "pages#home"
 end
