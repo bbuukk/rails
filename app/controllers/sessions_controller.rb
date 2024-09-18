7a17444d1299de1cf4df848ebf273a1a18901d34
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-09-18 11:55:06 +0300

    refactor: rename controller actions to comply CRUD

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def sign_up
+  def new
     @user = User.new
   end
 
-  def sign_in
+  def create
     user = User.find_by(email: params[:session][:email])
     if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in successfully'
     else
       @error = 'Invalid email or password. Try again.'
-      render :new_sign_in
+      render :new
     end
   end
 
-  def sign_out
+  def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: 'Logged out successfully'
   end
